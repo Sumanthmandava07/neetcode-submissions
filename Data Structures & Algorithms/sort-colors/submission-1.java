f514a2e53225d6ee6eac85d7df22ae1367f0b041
MANDAVA PURNA VENKATA SAI SUMANTH <sumanthmandava86@gmail.com>
2026-06-21 16:30:05 +0530

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int count1=0;
+        int count2=0;
+        int count3=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==0)
+            {
+            count1++;
+            }
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==1)
+            {
+                count2++;
+            }
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==2)
+            {
+                count3++;
+            }
+        }
+        for(int i=0;i<count1;i++)
+        {
+            nums[i]=0;
+        }
+        for(int i=count1;i<count1+count2;i++)
+        {
+            nums[i]=1;
+        }
+        for(int i=count1+count2;i<count1+count2+count3;i++)
+        {
+            nums[i]=2;
+        }
+    }
+}
