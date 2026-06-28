5099f8d0114fbc7518e1305b5ce48682209905c2
MANDAVA PURNA VENKATA SAI SUMANTH <sumanthmandava86@gmail.com>
2026-06-28 11:56:56 +0530

    Add: max-water-container - submission-4

---
+++ Data Structures & Algorithms/max-water-container/submission-4.java
@@ -0,0 +1,46 @@
+// class Solution {
+//     public int maxArea(int[] heights) {
+//         int left=0;
+//         int right=heights.length-1;
+//         int result=0;
+//         while(left<right)
+//         {
+//             int area=Math.min(heights[left],height[right]*(right-left));
+//             result=Math.max(result,area);
+//             if(heights[l]<=heights[r])
+//             {
+//                 l++;
+//             }
+//             else
+//             {
+//                 r--;
+//             }
+//         }
+//         return result;
+//     }
+// }
+
+class Solution
+{
+    public int maxArea(int[] height)
+    {
+        int n=height.length;
+        int maxwater=0;
+        int left=0;
+        int right=n-1;
+        while(left<=right)
+        {
+            int width=right-left;
+            int h=Math.min(height[left],height  [right]);
+            int area=width*h;
+            maxwater=Math.max(maxwater,area);
+            if(height[left]<height[right])
+            {
+                left++;
+            }
+            else
+                right--;
+        }
+        return maxwater;
+    }
+}
