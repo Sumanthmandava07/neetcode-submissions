9a4b9ab2416f1acb0d0c4dea371d8169a98315ec
MANDAVA PURNA VENKATA SAI SUMANTH <sumanthmandava86@gmail.com>
2026-07-08 21:41:17 +0530

    Add: move-zeroes - submission-3

---
+++ Data Structures & Algorithms/move-zeroes/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+     int i=0;
+    //  Arrays.sort(nums);
+    int temp=0;
+     for(int j=0;j<nums.length;j++)
+     {
+        if(nums[j]!=0)
+        {
+            temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+        }
+     }   
+    }
+}
