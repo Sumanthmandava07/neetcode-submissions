d647197ff6b5dcbde7b109e5a455641fa9ef2021
MANDAVA PURNA VENKATA SAI SUMANTH <sumanthmandava86@gmail.com>
2026-07-16 19:08:15 +0530

    Add: search-insert-position - submission-4

---
+++ Data Structures & Algorithms/search-insert-position/submission-4.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        // int n=nums.length;
+        int res=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i]==target)
+        //     {
+        //         res=i;
+        //         return res;
+        //     }
+        // }
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>=target)
+            {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
